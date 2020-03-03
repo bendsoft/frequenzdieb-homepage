f50b1f4b2e19d332d5b2fdae1838ab4ee0f88f39
ben <ben.dudler@gmail.com>
2020-03-04 00:55:43 +0100

    upgraded to java 11

--- frequenzdieb-server/build.gradle.kts
+++ frequenzdieb-server/build.gradle.kts
@@ -7,14 +7,14 @@ plugins {
 	id("com.palantir.docker") version "0.25.0"
 	id("org.springframework.boot") version "2.2.5.RELEASE"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
-	kotlin("jvm") version "1.3.61"
-	kotlin("plugin.spring") version "1.3.61"
-	kotlin("kapt") version "1.3.61"
+	kotlin("jvm") version "1.3.70"
+	kotlin("plugin.spring") version "1.3.70"
+	kotlin("kapt") version "1.3.70"
 }
 
 group = "ch.frequenzdieb"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 idea {
 	module {
@@ -41,6 +41,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
 	kapt("org.springframework.boot:spring-boot-configuration-processor")
 	runtimeOnly("org.springframework.boot:spring-boot-devtools")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
@@ -70,7 +71,7 @@ tasks {
 	withType<KotlinCompile> {
 		kotlinOptions {
 			freeCompilerArgs = listOf("-Xjsr305=strict")
-			jvmTarget = "1.8"
+			jvmTarget = "11"
 		}
 	}
 }
